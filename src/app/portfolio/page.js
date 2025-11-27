00c2928ccbd5e2b850c74e726120980632dca1ab
Ankit Kumar <ankitpandey841226@gmail.com>
2025-11-27 23:54:28 +0530

    fixed

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,7 +1,6 @@
-import { motion } from "framer-motion";
-import NavBar from "../components/NavBar";
-import FooterSection from "../components/FooterSection";
 import OurProject from "../../app/components/ourProjects";
+import FooterSection from "../components/FooterSection";
+import NavBar from "../components/NavBar";
 
 export const metadata = {
   title: "Portfolio at ArtofQR | Join Our Team",
@@ -16,7 +15,7 @@ export default function PortfolioPage() {
   return (
     <main className="min-h-screen bg-white dark:bg-gray-950">
       <NavBar />
-      
+
       <div className="mt-16 md:mt-20 lg:mt-24 px-4 md:px-8 lg:px-24">
         <OurProject />
       </div>
